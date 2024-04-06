ec106472c05a1e21ca1f57c15160d00b8613749b
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-05 21:53:09 -0400

    Add container

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useNavigate } from "react-router-dom"
 import NavBar from "./components/NavBar"
 import { useEffect } from "react";
+import { Container } from "@chakra-ui/react";
 
 function Layout() {
   const navigate = useNavigate();
@@ -12,7 +13,9 @@ function Layout() {
   return (
     <>
       <NavBar />
-      <Outlet />
+      <Container>
+        <Outlet />
+      </Container>
     </>
   )
 }
