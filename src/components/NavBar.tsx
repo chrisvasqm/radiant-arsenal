8f7ec5353ad0ebdd1d4915ebb4d2246ebedc865f
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-18 13:38:54 -0400

    Remove duplicated color attribute

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { Container, Typography } from '@mui/material'
 
 const NavBar = () => {
     return (
-        <div className='bg-zinc-900 py-2 mb-3 bg-zinc-800'>
+        <div className='py-2 mb-3 bg-zinc-800'>
             <Container>
                 <Typography variant='h1' fontSize={'2rem'} color={'white'}>Radiant Arsenal</Typography>
             </Container>
