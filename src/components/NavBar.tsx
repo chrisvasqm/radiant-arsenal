f6f08d7a19d1778a64d1e180d9760e668a4ecb16
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-18 13:39:11 -0400

    Add semantic HTML element

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,11 @@ import { Container, Typography } from '@mui/material'
 
 const NavBar = () => {
     return (
-        <div className='py-2 mb-3 bg-zinc-800'>
+        <nav className='py-2 mb-3 bg-zinc-800'>
             <Container>
                 <Typography variant='h1' fontSize={'2rem'} color={'white'}>Radiant Arsenal</Typography>
             </Container>
-        </div>
+        </nav>
     )
 }
 
