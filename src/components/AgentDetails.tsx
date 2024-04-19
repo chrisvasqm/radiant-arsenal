e3330ada0152340ee8c2fc7c06763a5ba95edaed
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-19 15:34:07 -0400

    Improve responsiveness on agent description

--- src/components/AgentDetails.tsx
+++ src/components/AgentDetails.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material'
+import { Grid, Stack, Typography } from '@mui/material'
 import { Agent } from '../models/Agent'
 import AgentTitle from './AgentTitle'
 import AgentAbilities from './AgentAbilities'
@@ -10,17 +10,21 @@ interface Props {
 const AgentDetails = ({ agent }: Props) => {
     return (
         <>
-            <Stack mt={2} direction={'row'}>
-                <img src={agent?.displayIcon} width={'20%'} height={'20%'} />
-                <Stack ml={2}>
-                    <Stack direction={'row'} gap={2}>
-                        <Typography fontWeight={'bold'} color={'#DA414E'}>{agent?.role.displayName}</Typography>
-                        <Typography>|</Typography>
-                        <AgentTitle displayName={agent?.displayName} />
+            <Grid container spacing={2} py={2}>
+                <Grid item xs={12} sm={5} md={4} sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <img src={agent?.displayIcon} width={'60%'} />
+                </Grid>
+                <Grid item xs={12} sm={7} md={8}>
+                    <Stack ml={2}>
+                        <Stack direction={'row'} gap={2}>
+                            <Typography fontWeight={'bold'} color={'#DA414E'}>{agent?.role.displayName}</Typography>
+                            <Typography>|</Typography>
+                            <AgentTitle displayName={agent?.displayName} />
+                        </Stack>
+                        <Typography>{agent?.description}</Typography>
                     </Stack>
-                    <Typography>{agent?.description}</Typography>
-                </Stack>
-            </Stack>
+                </Grid>
+            </Grid>
 
             <Stack mt={4}>
                 <Typography variant='h3' fontWeight={'bold'} color={'#DA414E'}>Abilities</Typography>
