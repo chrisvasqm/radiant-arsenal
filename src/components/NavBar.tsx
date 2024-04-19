f7dd829f2d04bb6e8ae54d431e110b79b94fa11a
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-19 15:48:23 -0400

    Add GitHub icon button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,14 @@
-import { Container, Typography } from '@mui/material'
+import { Container, IconButton, Typography } from '@mui/material'
+import { FaGithub } from 'react-icons/fa'
 
 const NavBar = () => {
     return (
         <nav className='py-2 mb-3 bg-zinc-800'>
-            <Container>
+            <Container sx={{ display: 'flex', justifyContent: 'space-between' }}>
                 <Typography variant='h1' fontSize={'2rem'} color={'white'}>Radiant Arsenal</Typography>
+                <IconButton aria-label='github' href='https://github.com/chrisvasqm/radiant-arsenal' target='_blank'>
+                    <FaGithub color='white' />
+                </IconButton>
             </Container>
         </nav>
     )
